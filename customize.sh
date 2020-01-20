ae4ec2b23e54828426963ba1e8e174601a5e86f7
zane <zane@fonsview.com>
2020-01-20 15:20:42 +0800

    fix customize.sh

--- customize.sh
+++ customize.sh
@@ -6,6 +6,6 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
-sed -i 's/192.168.1.1/192.168.31.100/g' package/base-files/files/bin/config_generate
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+git clone https://github.com/sypopo/luci-theme-atmaterial.git openwrt/package/lean/luci-theme-atmaterial
+sed -i 's/192.168.1.1/192.168.31.100/g' openwrt/package/base-files/files/bin/config_generate
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
