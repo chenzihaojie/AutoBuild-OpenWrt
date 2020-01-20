59cdf923d763a245ae6d41c0164f5cc7077ff92b
zane <zane@fonsview.com>
2020-01-20 15:07:09 +0800

    customize

--- customize.sh
+++ customize.sh
@@ -6,4 +6,6 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+sed -i 's/192.168.1.1/192.168.31.100/g' package/base-files/files/bin/config_generate
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
