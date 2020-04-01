6e4afc563f096dd69a0fc4dc638c02127292ff09
chenzihaojie <13294789+chenzihaojie@users.noreply.github.com>
2020-04-02 01:01:46 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,5 +7,5 @@
 #=================================================
 #1. Modify default IP
 git clone https://github.com/sypopo/luci-theme-atmaterial.git openwrt/package/lean/luci-theme-atmaterial
-sed -i 's/192.168.1.1/192.168.31.100/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
