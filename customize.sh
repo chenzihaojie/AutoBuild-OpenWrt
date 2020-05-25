84ed6d7372c60f3f407a30bf9fcb4f89c60402fb
zane <zane@fonsview.com>
2020-05-25 14:11:21 +0800

    update

--- customize.sh
+++ customize.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
-#=================================================
-#1. Modify default IP
-git clone https://github.com/sypopo/luci-theme-atmaterial.git openwrt/package/lean/luci-theme-atmaterial
+
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
